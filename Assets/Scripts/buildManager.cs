da0ef02177af73a5814f9fc88e7ddb76eabbc82b
alejandrodlsp <alejandrodlsp@hotmail.es>
2018-10-19 13:10:05 +0100

    Simple Build + Simple camera
    Basic build system, camera glimble and zoom

---
+++ Assets/Scripts/buildManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildManager : MonoBehaviour {
+
+	[SerializeField] private Camera theCamera;
+
+	[SerializeField] private GameObject spawnPrefab;
+
+	[SerializeField] private LayerMask snapPointLayerMask;
+	[SerializeField] private LayerMask raycastLayerMask;
+
+	void Start () {
+		theCamera = Camera.main;
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown(0))	// BUILD PART
+		{  // IF LEFT MOUSE BUTTON WAS PRESSED		
+
+			// IS THE MOUSE OVER A BLOCK?
+			Ray _ray = theCamera.ScreenPointToRay(Input.mousePosition); // Get ray from mouse position
+			RaycastHit _hitInfo; // Raycast out parameter
+
+			if (Physics.Raycast (_ray, out _hitInfo, raycastLayerMask)) { // Cast the ray and check for collision and OUT the data to _hitInfo, using the raycastLayerMask
+
+				// IF RAYCAST HIT IS A SNAP POINT
+				int _maskForHitObject = 1 << _hitInfo.collider.gameObject.layer; // MASKED VALUE FOR CLICKED LAYER		(00000000001 << 3rd Layer == 00000001000)
+				if ( (_maskForHitObject & snapPointLayerMask) > 0)  // COMPARE BITWISE IF LAYER CLICKED IS SNAP POINT LAYER MASK  (00000001000 & 00110001000 > 0)
+				{
+					buildPart(spawnPrefab, _hitInfo);	// BUILD PART
+				}
+			}
+		}
+	}
+
+	void buildPart(GameObject _prefab, RaycastHit _hit) {	// BUILD A PART GIVEN A HIT
+		//CLICKED SPAWN SPOT, INSTATIATE OBJECT
+		Vector3 _spawnSpot = _hit.collider.transform.position;    // POSITION OF CLICK COLLIDER
+		Quaternion _spawnRotation = _hit.collider.transform.rotation;   // GET ROTATION FOR SPAWN
+		GameObject _go = (GameObject)Instantiate(_prefab, _spawnSpot, _spawnRotation);   // INSTATIATE SPAWN PREFAB
+		_go.transform.SetParent(_hit.collider.transform, true);     // SET PARENT TO SNAP POINT CONSERVING POSITION
+
+		// DISABLE RENDERER AND COLLIDER ON SNAP POINT
+		_hit.collider.GetComponent<Renderer>().enabled = false;
+		_hit.collider.GetComponent<Collider>().enabled = false;
+	}
+
+}
---
+++ Assets/Scripts/cameraManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraManager : MonoBehaviour {
+
+	public Camera theCamera;
+	private Transform cameraRig;
+	private Vector3 lastMousePosition;
+
+	[SerializeField] private float orbitSensitivity = 1f;
+	[SerializeField] private float scrollSensitivity = 1f;
+	[SerializeField] private bool infiniteOrbit = false;
+	void Start () {
+		if (theCamera == null)	// CHECK FOR CAMERA
+			theCamera = GetComponent<Camera>();
+		if (theCamera == null)
+			theCamera = Camera.main;
+		if (theCamera == null)
+			Debug.LogError("COULD NOT FIND A CAMERA");
+
+		cameraRig = theCamera.transform.parent;	// GET CAMERA RIG FROM MAIN CAM PARENT
+	}
+
+	private void Update()
+	{
+		OrbitCamera();
+		ZoomCamera();
+	}
+	
+
+	void ZoomCamera()
+	{
+		float delta = Input.GetAxis("Mouse ScrollWheel");   // SCROLL DIFFERENCE AXIS
+
+		//Move camera backwards or foward based on valu of delta
+		Vector3 _changeOfPosition = theCamera.transform.forward / scrollSensitivity * delta;
+		theCamera.transform.Translate(_changeOfPosition);
+	}
+
+	void OrbitCamera () {
+
+		if (Input.GetMouseButtonDown(1)) // MOUSE WAS PRESSED ON THIS FRAME
+			lastMousePosition = Input.mousePosition;    // LOCK POSITION BEFORE MOVING
+
+		// IF DOWN RIGHT MOUSE BUTTON
+		if (Input.GetMouseButton(1)) {  
+			// CURRENT POSITION OF THE MOUSE ON SCREEN
+			Vector3 _currentMousePosition = Input.mousePosition;
+
+			// CHANGE POSITION IN PIXELS
+			Vector3 _mouseMovement = _currentMousePosition - lastMousePosition;
+
+			// ORBIT CAMERA RIG AROUND FOCAL POINT
+			Vector3 _rotationAngles = _mouseMovement / orbitSensitivity;
+			if(infiniteOrbit)
+				_rotationAngles *= Time.deltaTime;
+
+			theCamera.transform.RotateAround( cameraRig.position, theCamera.transform.right, _rotationAngles.y);
+			theCamera.transform.RotateAround( cameraRig.position, theCamera.transform.up, _rotationAngles.x);
+
+			// ORIENTATE CAMERA TO LOOK AT FOCAL POINT
+			Quaternion _lookRotation = Quaternion.LookRotation( - theCamera.transform.localPosition);
+			theCamera.transform.rotation = _lookRotation;
+
+			if(!infiniteOrbit)
+				lastMousePosition = _currentMousePosition;	// RESET MOUSE POSITION
+		}
+	}
+}
